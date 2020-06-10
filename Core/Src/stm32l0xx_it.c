34f5d9e18c3ca22db93b24a1742a089dedda9db3
evgenyagantaev <agantaev@mail.ru>
2020-06-10 14:58:15 +0300

    technical;

--- Core/Src/stm32l0xx_it.c
+++ Core/Src/stm32l0xx_it.c
@@ -1,35 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    stm32l0xx_it.c
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  *
-  * COPYRIGHT(c) 2019 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l0xx_hal.h"
 #include "stm32l0xx.h"
@@ -43,6 +11,9 @@
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim6;
 extern TIM_HandleTypeDef htim21;
+
+#include "ssd1306.h"
+#include "stdio.h"
 
 
 /******************************************************************************/
@@ -405,7 +376,6 @@ void USART1_IRQHandler(void)
 {
 	uint32_t isrflags   = USART1->ISR;
 	uint32_t cr1its     = USART1->CR1;
-	uint32_t cr3its;
 	uint32_t errorflags;
 
 	uint16_t usart_data;
@@ -434,5 +404,20 @@ void USART1_IRQHandler(void)
 
 			}
     	}
-  }  
+
+	}
+	else // some errors
+	{
+		ssd1306_SetCursor(0,0);
+		ssd1306_WriteString("usart1     ", Font_11x18, White);
+		ssd1306_SetCursor(0,22);
+		ssd1306_WriteString("error      ", Font_11x18, White);
+		ssd1306_UpdateScreen();
+
+		// clear error flags
+		uint32_t aux;
+		aux = USART1->ISR;
+		aux = USART1->RDR;
+		UNUSED(aux);
+	}
 }
